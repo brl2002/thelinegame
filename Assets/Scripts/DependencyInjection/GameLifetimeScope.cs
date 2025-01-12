c8a22b35f09dad10afd2753fdf30f7e55baead37
Minsoo Lee <06leemin@gmail.com>
2025-01-12 16:28:34 -0500

    Updated GameLifetimeScope to have RoadGuardBlockRowGroupFactory injection

--- Assets/Scripts/DependencyInjection/GameLifetimeScope.cs
+++ Assets/Scripts/DependencyInjection/GameLifetimeScope.cs
@@ -1,5 +1,6 @@
 using Core;
 using Game;
+using Road;
 using UnityEngine;
 using VContainer;
 using VContainer.Unity;
@@ -11,15 +12,17 @@ namespace DependencyInjection
         [SerializeField] private GameSettings m_GameSettings;
         [SerializeField] private GameEventContainer m_GameEventContainer;
         [SerializeField] private Player m_PlayerPrefab;
+        [SerializeField] private RoadGuardBlock m_RoadGuardBlockPrefab;
 
         protected override void Configure(IContainerBuilder builder)
         {
             m_GameSettings.Configure(Camera.main);
-        
+            
             builder.RegisterInstance(m_GameSettings);
             builder.RegisterInstance(m_GameEventContainer);
         
             builder.Register<PlayerFactory>(Lifetime.Scoped).WithParameter(typeof(Player), m_PlayerPrefab);
+            builder.Register<RoadGuardBlockRowGroupFactory>(Lifetime.Scoped).WithParameter(typeof(GameSettings), m_GameSettings).WithParameter(typeof(RoadGuardBlock),m_RoadGuardBlockPrefab);
         }
     }
 }
