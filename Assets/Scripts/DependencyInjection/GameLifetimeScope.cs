5fe3b07b4056e11ccd842fe6bfa8ced034232d03
Minsoo Lee <06leemin@gmail.com>
2025-01-13 21:29:33 -0500

    Also updated GameLifetimeScope class

--- Assets/Scripts/DependencyInjection/GameLifetimeScope.cs
+++ Assets/Scripts/DependencyInjection/GameLifetimeScope.cs
@@ -2,6 +2,7 @@ using Core;
 using Game;
 using Road;
 using UnityEngine;
+using UnityEngine.Serialization;
 using VContainer;
 using VContainer.Unity;
 
@@ -12,17 +13,20 @@ namespace DependencyInjection
         [SerializeField] private GameSettings m_GameSettings;
         [SerializeField] private GameEventContainer m_GameEventContainer;
         [SerializeField] private Player m_PlayerPrefab;
-        [SerializeField] private RoadGuardBlock m_RoadGuardBlockPrefab;
+        [SerializeField] private RoadBlock m_RoadBlockPrefab;
 
         protected override void Configure(IContainerBuilder builder)
         {
-            m_GameSettings.Configure(Camera.main);
+            Camera camera = Camera.main;
+            float screenHeight = camera.orthographicSize * 2;
+            float screenWidth = screenHeight * camera.pixelWidth / camera.pixelHeight;
+            m_GameSettings.Configure(screenHeight, screenWidth);
             
             builder.RegisterInstance(m_GameSettings);
             builder.RegisterInstance(m_GameEventContainer);
         
-            builder.Register<PlayerFactory>(Lifetime.Scoped).WithParameter(typeof(Player), m_PlayerPrefab);
-            builder.Register<RoadGuardBlockRowGroupFactory>(Lifetime.Scoped).WithParameter(typeof(GameSettings), m_GameSettings).WithParameter(typeof(RoadGuardBlock),m_RoadGuardBlockPrefab);
+            builder.Register<PlayerFactory>(Lifetime.Scoped).WithParameter(typeof(Player), m_PlayerPrefab).WithParameter(typeof(GameSettings), m_GameSettings);
+            builder.Register<RoadBlockRowGroupFactory>(Lifetime.Scoped).WithParameter(typeof(GameSettings), m_GameSettings).WithParameter(typeof(RoadBlock),m_RoadBlockPrefab);
         }
     }
 }
