672c98d5e23c075b3f576642890f22863e38f23c
Minsoo Lee <06leemin@gmail.com>
2025-01-17 23:35:25 -0500

    New GameConstants class for any constants

---
+++ Assets/Scripts/Game/GameConstants.cs
@@ -0,0 +1,15 @@
+namespace Game
+{
+    public static class GameConstants
+    {
+        public const int GAME_START_EVENT_INDEX = 0;
+        public const int GAME_RESET_EVENT_INDEX = 1;
+        public const int GAME_END_EVENT_INDEX = 2;
+        public const int GAME_PAUSE_EVENT_INDEX = 3;
+        public const int GAME_RESUME_EVENT_INDEX = 4;
+        public const int BOOSTER_ACTIVE_EVENT_INDEX = 5;
+        public const int BOOSTER_INACTIVE_EVENT_INDEX = 6;
+
+        public const string BEST_SCORE_PLAYER_PREFS_KEY = "BestScore";
+    }
+}
